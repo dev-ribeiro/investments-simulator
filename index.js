f220006e6133779069a01841bfdd53e3e258de75
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-07-02 18:14:38 -0300

    organize index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+// Dependencies
 require('dotenv').config()
 const express = require('express');
 const app = express();
+
+// Routes
 const calcsRoute = require('./routes/handleCalcs/handleCalcsRoute');
 
+// Application
 app.use('/api',calcsRoute);
 
+//Server
 app.listen(3444,(err)=>{
     if(!err) console.log('Server running on port 3444')
 })
