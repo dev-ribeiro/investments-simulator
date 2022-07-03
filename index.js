c6ab03e9ec914443254a7096514e41dd06fafb0b
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-07-03 08:09:40 -0300

    connection to mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Dependencies
-require('dotenv').config()
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
@@ -13,4 +14,11 @@ app.use('/api/calcs', calcsRoute);
 const PORT = process.env.PORT || 3334;
 app.listen(PORT, (err) => {
     if (!err) console.log('Server running on port ', PORT)
-})
+});
+
+//Database
+const url = process.env.URL;
+mongoose.connect(url);
+let db = mongoose.connection;
+db.on('error',()=>{console.log('Error')});
+db.once('open',()=>{console.log('Mongo DB connected')})
